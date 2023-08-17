1ae97bbb4986639b142c48709d8626f57976c8fb
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-17 14:38:31 -0500

    Installed tools and imported the web application framework (express)

---
+++ server.js
@@ -0,0 +1,14 @@
+//Install/Import web application framework,MongoDB object modeling tool  ;express,mongoose,dotenv
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+const PORT = 3000;
+app.listen (PORT, () => console.log(`Server running on port ${PORT}`))
